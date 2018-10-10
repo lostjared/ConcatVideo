45d22ffb6155358c3b6047fe18d540a175b8cbb8
lostjared <jaredbruni@gmail.com>
2018-10-10 14:49:33 -0700

    added outlets for controls and messages for actions

--- ConcatVideo/ConcatVideo/Controller.h
+++ ConcatVideo/ConcatVideo/Controller.h
@@ -4,9 +4,11 @@
 #import<Cocoa/Cocoa.h>
 
 @interface Controller : NSObject {
-    
+    IBOutlet NSButton *button_add,*button_remove,*button_concat;
+    IBOutlet NSTextField *conat_fps, *concat_w, *concat_h;
 }
-
+- (IBAction) addVideos: (id) sender;
+- (IBAction) removeVideo: (id) sender;
 - (IBAction) concatVideos: (id) sender;
 
 @end
--- ConcatVideo/ConcatVideo/Controller.mm
+++ ConcatVideo/ConcatVideo/Controller.mm
@@ -1,10 +1,14 @@
 #include "Controller.h"
-
 #include"CV.hpp"
-
 
 @implementation Controller
 
+- (IBAction) addVideos: (id) sender {
+    
+}
+- (IBAction) removeVideo: (id) sender {
+    
+}
 - (IBAction) concatVideos: (id) sender {
     cv::VideoWriter writer;
 }
