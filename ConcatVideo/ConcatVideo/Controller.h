fe1a1e2b2e4de3d0fbc1811ab73b9dc7738ea6d7
lostjared <jaredbruni@gmail.com>
2018-10-10 17:21:29 -0700

    updated interface / added table view delegate

--- ConcatVideo/ConcatVideo/Controller.h
+++ ConcatVideo/ConcatVideo/Controller.h
@@ -3,14 +3,24 @@
 
 #import<Cocoa/Cocoa.h>
 
+
+@interface TableDelegate : NSObject<NSTableViewDataSource, NSTableViewDelegate> {
+    
+}
+@property (strong,readwrite) NSMutableArray *values;
+@end
+
 @interface Controller : NSObject {
-    IBOutlet NSButton *button_add,*button_remove,*button_concat;
+    IBOutlet NSButton *button_add,*button_remove,*button_concat,*button_up, *button_down;
     IBOutlet NSTextField *conat_fps, *concat_w, *concat_h;
     IBOutlet NSTableView *table_view;
+    TableDelegate *table_delegate;
 }
 - (IBAction) addVideos: (id) sender;
 - (IBAction) removeVideo: (id) sender;
 - (IBAction) concatVideos: (id) sender;
+- (IBAction) moveUp: (id) sender;
+- (IBAction) moveDown: (id) sender;
 @end
 
 #endif
--- ConcatVideo/ConcatVideo/Controller.mm
+++ ConcatVideo/ConcatVideo/Controller.mm
@@ -1,7 +1,43 @@
 #include "Controller.h"
 #include"CV.hpp"
 
+@implementation TableDelegate
+
+@synthesize values;
+
+- (void) createValues {
+    values = [[NSMutableArray alloc] init];
+}
+
+- (id) tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex {
+    NSString *str =  [[aTableColumn headerCell] stringValue];
+    if( [str isEqualToString:@"Index"] ) {
+        NSString *s = [NSString stringWithFormat:@"%d",  (int)rowIndex+1, nil];
+        return s;
+    }
+    else if([str isEqualToString:@"Video File"]) {
+        return [values objectAtIndex: rowIndex];
+    }
+    return @"";
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView {
+    return [values count];
+}
+
+
+
+@end
+
 @implementation Controller
+
+- (void) awakeFromNib {
+    table_delegate = [[TableDelegate alloc] init];
+    [table_delegate createValues];
+    [table_view setDelegate: table_delegate];
+    [table_view setDataSource: table_delegate];
+    [table_view reloadData];
+}
 
 - (IBAction) addVideos: (id) sender {
     
@@ -11,6 +47,14 @@
 }
 - (IBAction) concatVideos: (id) sender {
     cv::VideoWriter writer;
+}
+
+- (IBAction) moveUp: (id) sender {
+    
+}
+
+- (IBAction) moveDown: (id) sender {
+    
 }
 
 @end
