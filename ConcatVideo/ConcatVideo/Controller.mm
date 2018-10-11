f97127c125dac579b0977d7eb63a645e8de83f83
lostjared <jaredbruni@gmail.com>
2018-10-10 17:34:06 -0700

    updated moveUp/moveDown etc

--- ConcatVideo/ConcatVideo/Controller.mm
+++ ConcatVideo/ConcatVideo/Controller.mm
@@ -40,21 +40,54 @@
 }
 
 - (IBAction) addVideos: (id) sender {
-    
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+    [panel setCanChooseFiles:YES];
+    [panel setCanChooseDirectories:NO];
+    [panel setAllowsMultipleSelection:YES];
+    [panel setAllowedFileTypes:[NSArray arrayWithObjects:@"mov", @"avi", @"mkv", @"m4v",nil]];
+    if([panel runModal]) {
+        for(int i = 0; i < [[panel URLs] count]; ++i) {
+            NSString *s = [[[panel URLs] objectAtIndex:i] path];
+            [table_delegate.values addObject:s];
+        }
+        [table_view reloadData];
+    }
 }
 - (IBAction) removeVideo: (id) sender {
-    
+    NSInteger row = [table_view selectedRow];
+    if(row >= 0) {
+        [table_delegate.values removeObjectAtIndex:row];
+        [table_view reloadData];
+    }
 }
 - (IBAction) concatVideos: (id) sender {
     cv::VideoWriter writer;
 }
 
 - (IBAction) moveUp: (id) sender {
-    
+    NSInteger index = [table_view selectedRow];
+    if(index > 0) {
+        NSInteger pos = index-1;
+        id obj = [table_delegate.values objectAtIndex:pos];
+        id mv = [table_delegate.values objectAtIndex:index];
+        [table_delegate.values setObject:obj atIndexedSubscript:index];
+        [table_delegate.values setObject:mv atIndexedSubscript: pos];
+        [table_view deselectAll:self];
+        [table_view reloadData];
+    }
 }
 
 - (IBAction) moveDown: (id) sender {
-    
+    NSInteger index = [table_view selectedRow];
+    if(index < [table_delegate.values count]-1) {
+        NSInteger pos = index+1;
+        id obj = [table_delegate.values objectAtIndex:pos];
+        id mv = [table_delegate.values objectAtIndex:index];
+        [table_delegate.values setObject:obj atIndexedSubscript:index];
+        [table_delegate.values setObject:mv atIndexedSubscript: pos];
+        [table_view deselectAll:self];
+        [table_view reloadData];
+    }
 }
 
 @end
